102dd228555bb55975c20c097cf305c73375f12d
Husameldin-thb <osman@th-brandenburg.de>
2023-12-20 20:01:41 +0100

    erstes Mal

---
+++ main.js
@@ -0,0 +1,71 @@
+
+document.querySelector("h1").style.color = "red";
+
+console.log("hosam");
+console.log(typeof[14,15,17])
+console.log(typeof true)
+console.log
+document.write("<h1>hallo world</h1>");
+document.querySelector("h1").style.color = "red";
+console.error('erroe');
+//window.print()   
+console.log(typeof"hosam");
+console.log(typeof"hosam", "osman");
+console.log(typeof[12,23,45]);
+console.log(typeof true)
+var user
+user = "hosam";
+user=10;
+console.log(user);
+let z = 10;
+z = 50,
+
+
+console.log(z);
+const v = 15;
+console.log(v); // bei const man kann die Variable nicht wieder ändern;
+
+document.write("<h2>hosam osman awas</h2>");
+
+document.querySelector("h2").style.fontSize = "70px"
+
+
+
+// concetnation: /////
+let name = "hosam";
+let age = 30;
+
+document.write(`mein name ist ${name}`); 
+//console.log(name + age);  // erste Option
+/* 
+man kann auch space als Variable machen.
+
+let name = "hosam";
+let age = 30;
+let x = " ";
+console.log(name + x + age); // zweite Option 
+*/
+//console.log(`meine Name ist ${name} and i am ${age} `) // beste option
+
+console.log(`Mein Name ist ${name} und mein Alter ist ${age}`); 
+
+console.log(5+4);
+//comsole.log('mein name ist Hosaqm ${nmm,e ofremv ok idenv  jonv  iojernvüö,xm vcioubn edoöö dcijn idkcn  ikn c }')
+
+document.querySelector("h1").style.color = 'blue';
+
+name= "Abo"
+age= 38
+console.log(`mein Name ist ${name} und ich bin ${age}`)
+
+document.write(`mein name ist ${name}`);
+
+console.log(typeof true);
+console.log(typeof"hosam", "osman");
+console.log(typeof[12,15,20]);
+console.log(typeof false);
+window.alert("hallo hosam");
+console.table({firstname:"John", lastname:"Doe"});
+
+
+
